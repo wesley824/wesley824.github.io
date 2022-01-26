034e1ef02477217096b22990b16f5b0d68dca3a2
wesley824 <linwesley824@gmail.com>
2022-01-26 12:53:02 -0800

    Update
    Updated descriptions and added images

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Navigation Button
 const navToggle = document.querySelector('.nav_toggle');
 const navLinks = document.querySelectorAll('.nav_link')
 
@@ -10,3 +11,24 @@ navLinks.forEach(Link => {
         document.body.classList.remove('nav_open');
     })
 })
+
+
+// Get the button:
+const topButton = document.getElementById("top_button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
